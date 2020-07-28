a2453dac486f1fe03cd7301ecb738869b0c0e121
Mazurr <bartoszmazur98@gmail.com>
2020-07-28 19:19:02 +0200

    Add Logging system using sdplog

--- FTEngine2D/src/FTEngine2D/EntryPoint.h
+++ FTEngine2D/src/FTEngine2D/EntryPoint.h
@@ -6,6 +6,11 @@ extern FTE::Application* FTE::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	FTE::Log::Init();
+	FTE_CORE_WARN("Initialized Log!");
+	int a = 5;
+	FTE_INFO("Hello! var={0}", a);
+
 	auto app = FTE::CreateApplication();
 	app->Run();
 	delete app;
---
+++ FTEngine2D/src/FTEngine2D/Log.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace FTE {
+	class FTE_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros
+#define FTE_CORE_TRACE(...)    ::FTE::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define FTE_CORE_INFO(...)     ::FTE::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define FTE_CORE_WARN(...)     ::FTE::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define FTE_CORE_ERROR(...)    ::FTE::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define FTE_CORE_CRITICAL(...) ::FTE::Log::GetCoreLogger()->critical(__VA_ARGS__)
+								 
+// Client log macros			 
+#define FTE_TRACE(...)         ::FTE::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define FTE_INFO(...)          ::FTE::Log::GetClientLogger()->info(__VA_ARGS__)
+#define FTE_WARN(...)          ::FTE::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define FTE_ERROR(...)         ::FTE::Log::GetClientLogger()->error(__VA_ARGS__)
+#define FTE_CRITICAL(...)      ::FTE::Log::GetClientLogger()->critical(__VA_ARGS__)
